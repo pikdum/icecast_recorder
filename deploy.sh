55d1ff4886ccc697c676fdfdb6ade19f6f8b936f
pikdum <pikdum@kuudere.moe>
2024-09-28 16:47:51 -0500

    feat: fork for more generic icecast

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-scp record-stream.sh root@files.usagi.zone:/usr/local/bin/
-scp record-stream.service root@files.usagi.zone:/etc/systemd/system/
-ssh root@files.usagi.zone 'systemctl daemon-reload && systemctl restart record-stream'
+scp record-stream.sh root@files.usagi.zone:/usr/local/bin/record-shamiradio.sh
+scp record-stream.service root@files.usagi.zone:/etc/systemd/system/record-shamiradio.service
+ssh root@files.usagi.zone 'systemctl daemon-reload && systemctl restart record-shamiradio'
--- record-stream.sh
+++ record-stream.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+name="shamiradio"
+api="https://shamiradio.imgoodatth.is/status-json.xsl"
+stream="https://shamiradio.imgoodatth.is/main.mp3.m3u"
+
 is_streaming() {
     [ "$DEBUG" = true ] && [ -e "is_streaming" ] && return 0
-    echo "$data" | jq -e '.main.isafkstream == false' >/dev/null
+    echo "$data" | jq -e '.icestats.source != null' >/dev/null
 }
 
 is_recording() {
@@ -12,11 +16,11 @@ is_recording() {
 }
 
 start_recording() {
-    recording="r-a-dio_$(date +"%Y-%m-%dT%H-%M-%S-%3N").mp3"
+    recording="${name}_$(date +"%Y-%m-%dT%H-%M-%S-%3N").mp3.m3u"
     log="${recording}.log"
-    wget https://stream.r-a-d.io/main.mp3 -O "$recording" >/dev/null 2>&1 &
+    wget "$stream" -O "$recording" >/dev/null 2>&1 &
     pid=$!
-    start_time=$(echo "$data" | jq -r '.main.current')
+    echo "$data" | jq -r '.icestats.source | "Server Name: \(.server_name)\nServer Description: \(.server_description)\nGenre: \(.genre)"' >>"$log"
 }
 
 stop_recording() {
@@ -24,36 +28,23 @@ stop_recording() {
 }
 
 log_songs() {
-    echo "$data" | jq -r --arg start_time "$start_time" '.main.lp[] | select(.timestamp >= ($start_time | tonumber)) | "\(.timestamp): \(.meta)"' >>"$log"
-    echo "$data" | jq -r '.main | "\(.end_time): \(.np)"' >>"$log"
-    sort -u -o "$log" "$log"
-    awk -F ': ' '{ lines[tolower($2)] = $0 } END { for (i in lines) { print lines[i] } }' "$log" >/tmp/song-log && mv /tmp/song-log "$log"
-    sort -u -o "$log" "$log"
-}
-
-log_dj() {
-    new_dj=$(echo "$data" | jq -r '.main.dj.djname')
-    if [ "$new_dj" != "$old_dj" ]; then
-        echo "$(date +%s): *DJ* $new_dj" >>"$log"
-    fi
-    old_dj=$new_dj
+    echo "$data" | jq -r '.icestats.source | "\(.title)"' >>"$log"
+    sort --merge --unique "$log" -o "$log"
 }
 
 trap stop_recording EXIT
 
 while true; do
-    data=$(curl -s https://r-a-d.io/api)
+    data=$(curl -s "$api")
     if ! is_recording && is_streaming; then
         start_recording
         echo "Recording started: $recording"
-        log_dj
         log_songs
     elif is_recording && ! is_streaming; then
         stop_recording
         echo "Recording stopped: $recording"
         log_songs
     elif is_recording && is_streaming; then
-        log_dj
         log_songs
     fi
     sleep 60
