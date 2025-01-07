8e0116cc9f4c7db926cb71318bedbb8c1c82d115
pikdum <pikdum@kuudere.moe>
2025-01-06 20:35:20 -0600

    chore: change servers

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-scp record-stream.sh root@files.usagi.zone:/usr/local/bin/record-shamiradio.sh
-scp record-stream.service root@files.usagi.zone:/etc/systemd/system/record-shamiradio.service
-ssh root@files.usagi.zone 'systemctl daemon-reload && systemctl restart record-shamiradio'
+scp record-stream.sh root@yui.usagi.zone:/usr/local/bin/record-shamiradio.sh
+scp record-stream.service root@yui.usagi.zone:/etc/systemd/system/record-shamiradio.service
+ssh root@yui.usagi.zone 'systemctl daemon-reload && systemctl restart record-shamiradio'
